4be2159eb41c3ddf8045a445562f50b99df60910
Anshu Sarkar <anshusarkar11@gmail.com>
2022-10-12 23:00:42 +0530

    hello

---
+++ HelloJava.java
@@ -0,0 +1,14 @@
+class HelloJava{
+    public static void main(String[] args) { //This is main function in java whic is called main method
+        System.out.println("HelloJava!");
+        Zero();
+        Methods();
+    }
+    static void Zero(){
+        System.out.println("Hello Methods !");//This is like user defined functions in java whic is called normal methods
+    }
+    static void Methods(){
+        System.out.println("Methods = User defined functions in OOP with extended capabilities");
+    }
+    
+}
---
+++ zero.java
@@ -0,0 +1,8 @@
+public class zero  {
+     static void Alpha() {
+        System.out.println("Hello world !");
+    }
+    public static void main(String[] args) {
+        Alpha();
+    }
+}
