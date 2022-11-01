c3d66131d9f49b3ea4300570a37e7cec177d8f39
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-01 19:59:13 +0530

    Echo

--- CallByreference.java
+++ CallByreference.java
@@ -1,6 +1,6 @@
 class CallByreference {
     int data = 50 ;
-    void change(CallByreference cr )
+    void change(CallByreference cr ) // had to pass the value in the method one is the Class name other is the object of the class  
     {
         cr.data = cr.data + 100;
     }
@@ -9,7 +9,7 @@ class CallByreference {
         CallByreference cr = new CallByreference(); //cr is the object of the class CallByreference 
         
         System.out.println("Before change "+ cr.data);
-        cr.change(cr);
+        cr.change(cr);                              //Had to pass the object of the class in the method as a parameter                  
         System.out.println("After change "+ cr.data);
     }
 }
