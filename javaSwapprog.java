e6b1d46df9bef66150e18bbc33a0909d766576fa
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-05 08:29:13 +0530

    Echo

---
+++ javaSwapprog.java
@@ -0,0 +1,13 @@
+class javaSwapprog{
+    public static void main (String []args){
+        int a = 10;
+        int b = 20;
+        System.out.println("The initial values are : " +a + " and " +b);
+        int temp ;
+        temp = a+b;
+        a = temp-a;
+        System.out.println("The value of a before swapping is :"+a);
+        b = temp-a;
+        System.out.println("The value of b before swapping is :"+b);
+    }
+}
