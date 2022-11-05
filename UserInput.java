fc93eb031e74bcfaeff83f051facf347f669dcda
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-05 09:01:16 +0530

    Echo

--- UserInput.java
+++ UserInput.java
@@ -1,9 +1,18 @@
 import java.util.*;
 class UserInput {
-    public static void main(String [] args){
+    public static void main(String [] args){                                
+        
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter a name : ");
-        String name = inp.nextLine();
-        System.out.println("This is the entered name : "+name);
+        String name = inp.nextLine();                                       /*nextBoolean()	Reads a boolean value from the user*/
+        System.out.println("This is the entered name : "+name);            /*  nextByte() Reads a byte value from the user
+                                                                            nextDouble()	Reads a double value from the user
+                                                                            nextFloat()	Reads a float value from the user
+                                                                            nextInt()	Reads a int value from the user
+                                                                            nextLine()	Reads a String value from the user
+                                                                            nextLong()	Reads a long value from the user
+                                                                            nextShort() */
+        
+        
     }
 }
