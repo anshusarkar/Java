4e3d6fbf418daa95c87a964f72cd46807e31ed5a
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-05 08:19:41 +0530

    Echo

--- factorialinmainmethod.java
+++ factorialinmainmethod.java
@@ -1,8 +1,8 @@
 class factorialinmainmethod {
     public static void main (String [] args ){
-        int i=1 ; num=10;
-        while(nume!=0){
-            i=i*num--
+        int i=1 ; int num=10;
+        while(num!=0){
+            i=i*num--;
         }
         System.out.println(i);
     }
