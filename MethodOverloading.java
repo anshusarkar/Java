c318c4620093fadf7c92b4c72325213cf7067576
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-05 19:49:14 +0530

    Echo

---
+++ MethodOverloading.java
@@ -0,0 +1,17 @@
+class MethodOverloading{ 
+    public static void main(String[] args){
+         
+            System.out.println(add(1,2 ,3));
+    }
+    static double add (int i , int j){
+        System.out.println("Sum is done in between two integers");  //Some how that is returing an double as output
+        return (i+j);                                              
+    }
+    static double add (int i , int j , int k){
+        System.out.println("Sum is done between Three integers"); 
+        return (i+j+k) ;
+    }
+    
+    //Conclusion as here sam method is gettion loaded with multiple data types the process is called method over loading 
+
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,9 +1,10 @@
-class factorialinmainmethod {
-    public static void main (String [] args ){
-        int i=1 ; num=10;
-        while(nume!=0){
-            i=i*num--
-        }
-        System.out.println(i);
+class MethodOloading{
+    public static void main(String[] args){
+        MethodOverloading obj = new MethodOverloading(); //Creating a Defualt Constroctor
+            System.out.println(add(1,2));
+    }
+    static double add (int i , int j){
+        System.out.println("Sum is done in between two integers");
+        return (i+j);
     }
 }
