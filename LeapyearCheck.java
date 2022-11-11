4a0c19a84eb6efe94aeac548d16f2d46808fc1b9
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-11 19:59:55 +0530

    echo

--- LeapyearCheck.java
+++ LeapyearCheck.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class LeapyearCheck {
     public static void main (String [] args){
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Enter a year : ");
-        int year = inp.nextInt();
-        if(year%400==0||year%100!=0||year%4==0){
-            System.out.println("Entered year is leap year");
-        }
-        else{
-            System.out.println("Entered year is not a leap year");
+        try (Scanner inp = new Scanner(System.in)) { //Had to use try statement other wise it was showing a memeory leak error
+            System.out.print("Enter a year : ");
+            int year = inp.nextInt();
+            if(year%400==0||year%100!=0||year%4==0){
+                System.out.println("Entered year is leap year");
+            }
+            else{
+                System.out.println("Entered year is not a leap year");
+            }
         }
 
 
