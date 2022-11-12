cae74d2a6bb53efde573739cbabb3f35ccbd5990
Anshu Sarkar <anshusarkar11@gmail.com>
2022-11-12 08:26:47 +0530

    echo

---
+++ p1/classPackage.java
@@ -0,0 +1,8 @@
+package p1 ;
+public class classPackage {
+    public void dsiplay ()
+    {
+      System.out.println("This is a package ");
+    }
+   
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+package p1 ;
+public class x {
+    public void display()
+    {
+        System.out.println("This is a package");
+    }
+}
