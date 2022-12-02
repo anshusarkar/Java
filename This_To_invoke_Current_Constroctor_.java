8499715918dffcd7c5b386c98cb5bc2ca2b3c060
Anshu <anshusarkar11@gmail.com>
2022-12-02 16:47:51 +0530

    echo

--- This_To_invoke_Current_Constroctor_.java
+++ This_To_invoke_Current_Constroctor_.java
@@ -3,12 +3,13 @@ class This_To_invoke_Current_Constroctor_{
      This_To_invoke_Current_Constroctor_(){ //Calling no argument constructor from parameterized constructor
         System.out.println("This is from the non parametarized constructor");     
     }
-    This_To_invoke_Current_Constroctor_(int x){
+    This_To_invoke_Current_Constroctor_(int x){//Without this() only the old defination of the construcor would get executed 
         this();
         System.out.println(x);
     }
 
     public static void main(String[] args) {
         This_To_invoke_Current_Constroctor_ ob = new This_To_invoke_Current_Constroctor_(10);
+        
     }
 }
