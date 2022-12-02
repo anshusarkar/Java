b8f6121c242d814c543b234429b244dcb40cafc2
Anshu <anshusarkar11@gmail.com>
2022-12-02 20:09:19 +0530

    echo

--- Interface.java
+++ Interface.java
@@ -1,13 +1,10 @@
 interface An_interface { 
      default void A_method(){   
-    }
-}
-
-
-class Interface implements An_interface {                                 
-    public void A_method(){
         System.out.println("This method is from an interface !");
     }
+}
+class Interface implements An_interface {                                 
+    
     public static void main(String[] args) {
         An_interface in ;
         Interface Inter = new Interface();
