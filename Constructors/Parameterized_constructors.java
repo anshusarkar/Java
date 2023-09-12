aec4fb3c721b39a0dd1397bcc7b40d4c67a4cc5b
Anshu <anshusarkar11@gmail.com>
2023-09-12 21:11:03 +0530

    echo

--- Constructors/Parameterized_constructors.java
+++ Constructors/Parameterized_constructors.java
@@ -2,16 +2,16 @@ package Constructors;
 
 public class Parameterized_constructors {
 
-    int value; 
-    float value2;
+    int value_to_be_used_by_methods; 
+    float value2_to_be_used_by_methods;
 
     Parameterized_constructors(int value , float value2){
-       this.value = value;
-       this.value2 = value2;
+       this.value_to_be_used_by_methods = value;
+       this.value2_to_be_used_by_methods = value2;
     }
 
     void show(){
-        System.out.println("The value that is passed is " + this.value + ", " + this.value2 + ".");
+        System.out.println("The value that is passed is " + this.value_to_be_used_by_methods + ", " + this.value2_to_be_used_by_methods + ".");
     }
 
     public static void main(String[] args) {
