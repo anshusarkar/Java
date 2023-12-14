5a5a67bcf78a79830a7392286ea5c1633e0afd02
Anshu <anshusarkar11@gmail.com>
2023-12-14 18:09:17 +0530

    echo

--- thread2.java
+++
@@ -1,9 +0,0 @@
-class therad2 extends Thread {
-    public void run(){
-        System.out.println("This is a thread run !");  
-    }
-    public static void main(String[] args) {
-        therad2 t = new therad2();
-        t.start();
-    }
-}
