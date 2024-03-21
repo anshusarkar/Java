8f533368536c7fff5a6a938a067a47ecee074262
anshusarkar <anshusarkar11@gmail.com>
2024-03-21 21:44:13 +0530

    echo

---
+++ Getters and setters/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+    private String name;
+    private int age;
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        p.setName("Anshu");
+        p.getName();
+        System.out.println(p.name);
+
+    }
+}
+
